183b52fc63e4fbc2f37d8b3e37e6e230a85c0775
Fabien Brocklesby <fabienbrocklesby@icloud.com>
2020-12-04 06:45:49 +1300

    Update index.js

--- index.js
+++ index.js
@@ -70,16 +70,19 @@ function addChat(input, product) {
   let userDiv = document.createElement("div");
   userDiv.id = "user";
   userDiv.className = "user response";
+  userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
   messagesContainer.appendChild(userDiv);
 
   let botDiv = document.createElement("div");
   let botImg = document.createElement("img");
   let botText = document.createElement("span");
   botDiv.id = "bot";
+  botImg.src = "bot-mini.png";
   botImg.className = "avatar";
   botDiv.className = "bot response";
   botText.innerText = "Typing...";
   botDiv.appendChild(botText);
+  botDiv.appendChild(botImg);
   messagesContainer.appendChild(botDiv);
   // Keep messages at most recent
   messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
